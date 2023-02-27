571fde9af97fe856057890fe6fc760c89596f64d
xtremeqg <xtremeq_g@hotmail.com>
2023-02-27 22:35:41 +0200

    Fixed 64-bit builds creating corrupt Jonty tab files

--- src/pngpal2raw.cpp
+++ src/pngpal2raw.cpp
@@ -429,7 +429,7 @@ struct SmallSpriteV1 {
  */
 struct JontySpriteV1 {
     /** Offset of the sprite data in DAT file. */
-    size_t Data;
+    uint32_t Data;
     /** Width of the sprite data. */
     unsigned char SWidth;
     /** Height of the sprite data. */
